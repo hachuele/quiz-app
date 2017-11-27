268e7dfc6228341f420be866ff67df27373e2708
Eric Hachuel <hachuel.e@gmail.com>
2017-11-26 20:15:51 -0800

    Added error functions to the functions file and turned on output buffering on initialize.php

--- private/functions.php
+++ private/functions.php
@@ -20,4 +20,19 @@
         return urlencode($string);
     }
 
+    function error_404(){
+        header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+        exit();
+    }
+
+    function error_500(){
+        header($_SERVER["SERVER_PROTOCOL"] . " 500 Internal Server Error");
+        exit();
+    }
+
+    function redirect_to($location) {
+        header("Location: " . $location);
+        exit;
+    }
+
 ?>
--- private/initialize.php
+++ private/initialize.php
@@ -1,4 +1,5 @@
 <?php
+    ob_start(); //output buffering on
     // Assign file paths to PHP constants
     // __FILE__ returns the current path to this file
     // dirname() returns the path to the parent directory
