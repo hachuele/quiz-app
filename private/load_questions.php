77f8321cea0f80193b39ca2b75804efd0531fd32
Eric Hachuel <erichachuel@Erics-MBP.attlocal.net>
2018-01-25 01:04:22 -0800

    new load questions and process answers files

---
+++ private/load_questions.php
@@ -0,0 +1,22 @@
+<?php require_once('initialize.php'); ?>
+<?php
+
+/******************************************************************
+ * DESCRIPTION: Retrieves questions and choices from the database
+ * for display. (on Next)
+ *                             ----
+ * @author: Eric J. Hachuel
+ * University of Southern California, High-Performance Computing
+ ******************************************************************/
+
+
+/**
+-DATA: CURRENT QUESTION, THE ASSESSMENT ID
+-RETURN: NEXT QUESTION DETAILS (NAME, CHOICES, ETC. FOR UPDATE)
+
+*/
+
+
+
+
+?>
--- private/process_answers.php
+++ private/process_answers.php
@@ -1,17 +1,32 @@
+<?php require_once('initialize.php'); ?>
 <?php
+
 
 /******************************************************************
  * DESCRIPTION: Processes user submitted answers through the Ajax
- * calls, returns answer details as data to the user.
+ * calls, returns answer details as data to the user, stores
+ * user answers.
  *                             ----
  * @author: Eric J. Hachuel
  * University of Southern California, High-Performance Computing
  ******************************************************************/
 
+/**
+-DATA: WHAT THE USER SELECTS (CHOICES), THE QUESTION, THE ASSESSMENT ID
+-PERFORMS: ADDS THE DATA TO THE TABLE FOR THE SPECIFIC USER
+-RETURN: THE ANSWER DETAILS FOR DISPLAY
+
+*/
+
+//INITIALIZE VARIABLES
+$data = array();
+
+//used to add Classes when answers submitted
+$data['correct-glyphicon-class'] = 'glyphicon-ok solution_glyphicon_correct';
+$data['incorrect-glyphicon-class'] = 'glyphicon-remove solution_glyphicon_incorrect';
 
 
-
-
+//$assessment_id = $_POST['assessment_id'];
 
 
 ?>
