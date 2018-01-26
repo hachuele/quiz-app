56ed8c23b5eeaad1c7d85c47ce62a97ca2e3a4c2
Eric Hachuel <erichachuel@Erics-MBP.attlocal.net>
2018-01-26 00:06:31 -0800

    removed old static code and replaced it with php to generate the question data

--- public/js/quizz-script.js
+++ public/js/quizz-script.js
@@ -5,49 +5,6 @@
 
 
 
-/******************************************************
-* THE HIDE QUESTION CARD FUNCTION HIDES QUESTIONS
-*******************************************************/
-function hideQuestionCard(){
-
-}
-
-
-/******************************************************
-* THE DISABLE FORM INPUT FUNCTION DISABLES ALL INPUTS
-*******************************************************/
-function disableFormInput(){
-    //Disable buttons and elements on click
-    $("#view_answers_btn").prop('disabled', true);
-    //Disable all checkbox/radio elements
-    $("#question_form :input").prop("disabled", true);
-}
-
-
-/******************************************************
-* ENABLE NEXT CHECKS ENABLES MOVING TO THE NEXT QUESTION
-* OR SUBMITTING THE QUIZZ IF ALL QUESTIONS ANSWERED
-*******************************************************/
-//TODO: MUST USE DATABASE INFORMATION TO KNOW IF LAST QUESTION
-function enableNext(){
-    //Enable the next question (if any) //THIS WILL NEED TO BE BASED ON NUM QUESTIONS
-    $("#next_question_btn").prop('disabled', false);
-}
-
-/******************************************************
-* LOAD NEXT PERFORMS AN AJAX CALL TO RETRIEVE THE NEXT
-* QUESTION DATA (IF AVAILABLE)
-*******************************************************/
-function loadNext(){
-    if($("#next_question_btn").is(":enabled")){
-
-        //THE DATA SHOULD BE THE QUESTION NUMBER
-        //AJAX CALL TO LOAD QUESTIONS
-
-    }
-
-
-}
 
 //WILL  USE THE QUESTION NUMBER !!!!!!
 //var active = 1;
@@ -64,23 +21,12 @@ function loadNext(){
 //}
 
 
-
-/******************************************************
-* LOAD PREVIOUS PERFORMS AN AJAX CALL TO RETRIEVE THE
-* PREVIOUS' QUESTION DATA (IF AVAILABLE)
-*******************************************************/
-function loadPrevious(){
-    if($("#previous_question_btn").is(":enabled")){
-
-        //THE DATA SHOULD BE THE QUESTION NUMBER
-        //AJAX CALL TO LOAD QUESTIONS
-
-    }
-
-
-}
+//USE JQUERY REGEX TO FIND ACTIVE QUESTION NUMBER
 
 //PROBABLY WILL NOT NEED TO RESET PAGE!!!
+
+//NOTE QUESTION FORM WILL CHANGE TO question_form_1...etc so won't reset.
+
 /******************************************************
 * THE RESET QUIZZ PAGE FUNCTION UPDATES/RESTORES THE
 * FORM TO ITS ORIGINAL FORMAT.
@@ -88,7 +34,7 @@ function loadPrevious(){
 function resetQuizzPage(){
     $("#answer_explanations_div").hide();
     //RESET THE FORM
-    $('#question_form')[0].reset();
+//    $('#question_form')[0].reset();
     //DISABLE VIEW ANSWERS AND SUBMIT
     $("#view_answers_btn").attr('disabled', true);
     $("#submit_answers_btn").attr('disabled', true);
@@ -99,13 +45,12 @@ function resetQuizzPage(){
 
 /******************code runs once DOM ready******************/
 $(document).ready(function(){
-
-    //Current question
-    var currentQuestion = 0;
-
     //Reset the quizz page then update the footer
     resetQuizzPage();
     footerUpdate();
+
+    // get current active question number (class ACTIVE) REGEX
+    var currentQuestion = 1;
 
 
     /******************************************************
@@ -136,13 +81,84 @@ $(document).ready(function(){
         }
     }
 
-    //NEED TO ADD CHOICE ID (AND EVERYTHING ELSE ON THE FUNCTIONS PAGE)
+    //NEED TO ADD QUESTION ID (AND EVERYTHING ELSE ON THE FUNCTIONS PAGE)
     /******************************************************
-    * ENABLE ANSWER BUTTON ON INPUT CLICK
+    * ENABLE ANSWER BUTTON ON INPUT CLICK (CHECKBOXES)
     *******************************************************/
     $('.checkbox_item').click(function () {
         $('#view_answers_btn').attr('disabled', !$('.checkbox_item:checked').length);
     });
+
+
+    /******************************************************
+    * ENABLE ANSWER BUTTON ON INPUT CLICK (CHECKBOXES)
+    *******************************************************/
+    $('.radio_item').click(function () {
+        $('#view_answers_btn').attr('disabled', false);
+    });
+
+
+
+    /******************************************************
+    * ENABLE NEXT CHECKS ENABLES MOVING TO THE NEXT QUESTION
+    * OR SUBMITTING THE QUIZZ IF ALL QUESTIONS ANSWERED
+    *******************************************************/
+    //TODO: MUST USE DATABASE INFORMATION TO KNOW IF LAST QUESTION
+    function enableNext(){
+        //Enable the next question (if any) //THIS WILL NEED TO BE BASED ON NUM QUESTIONS
+        $("#next_question_btn").prop('disabled', false);
+    }
+
+    /******************************************************
+    * LOAD NEXT PERFORMS AN AJAX CALL TO RETRIEVE THE NEXT
+    * QUESTION DATA (IF AVAILABLE)
+    *******************************************************/
+    function loadNext(){
+        if($("#next_question_btn").is(":enabled")){
+
+            //THE DATA SHOULD BE THE QUESTION NUMBER
+            //AJAX CALL TO LOAD QUESTIONS
+
+        }
+
+
+    }
+
+
+    /******************************************************
+    * LOAD PREVIOUS PERFORMS AN AJAX CALL TO RETRIEVE THE
+    * PREVIOUS' QUESTION DATA (IF AVAILABLE)
+    *******************************************************/
+    function loadPrevious(){
+        if($("#previous_question_btn").is(":enabled")){
+
+            //THE DATA SHOULD BE THE QUESTION NUMBER
+            //AJAX CALL TO LOAD QUESTIONS
+
+        }
+
+
+    }
+
+
+    /******************************************************
+    * THE HIDE QUESTION CARD FUNCTION HIDES QUESTIONS
+    *******************************************************/
+    function hideQuestionCard(){
+
+    }
+
+
+
+    /******************************************************
+    * THE DISABLE FORM INPUT FUNCTION DISABLES ALL INPUTS
+    *******************************************************/
+    function disableFormInput(){
+        //Disable buttons and elements on click
+        $("#view_answers_btn").prop('disabled', true);
+        //Disable all checkbox/radio elements
+        $("#question_form :input").prop("disabled", true);
+    }
 
 
 
--- public/quizz/index.php
+++ public/quizz/index.php
@@ -1,3 +1,7 @@
+<?php
+    //start the session
+    session_start();
+?>
 <?php require_once('../../private/initialize.php'); ?>
 
 <!--STATIC VARIABLES FOR CURRENT PAGE-->
@@ -8,6 +12,8 @@
 ?>
 
 
+
+
 <!--DATA FROM SQL DATABASE-->
 <?php
 
@@ -16,33 +22,27 @@
 
     //ALSO NEED TO PASS USER ID TO DETERMINE NUM QUESTIONS ALREADY COMPLETED ETC ETC - LOAD EXISTING DATA
 
-
     $assessment_id = $_GET['assessment_id'] ?? '1'; // get the assessment id from url
     $assessment_name = get_assessment_name($assessment_id); // get the name of this assessment for display
 
 
-    $current_q_num = 1; //Set current question number to 1
-
-
     $question_set = find_questions_by_assessment_id($assessment_id); //get questions for selected assessment
     $num_questions = mysqli_num_rows($question_set); //get the number of questions in the set
-    //add question set to array for future manipulation
-    while($question = mysqli_fetch_array($question_set, MYSQLI_BOTH)){
-        array_push($questions_array, $question);
-    }
 
 
-    //When the user clicks next
-    if(isset($_POST['submit'])){
+//    //add question set to array for future manipulation
+//    while($question = mysqli_fetch_array($question_set, MYSQLI_BOTH)){
+//        array_push($questions_array, $question);
+//    }
 
+//    $choice_set = find_choices_by_question_id(1);
+//    $choices_array = array();
+//     while($choice = mysqli_fetch_array($choice_set, MYSQLI_BOTH)){
+////        array_push($questions_array, $question);
+//         echo $choice['question_choice_text'];
+//    }
 
-
-
-    }
-
-
-
-
+    //use choice id for in the choice while loop (give an id to the check or radio name)
 
 
 
@@ -87,36 +87,51 @@
 </div>
 
 <div class="container main_content">
-
-    <div id="quizz_question_div" class="container question_card">
-        <div id="quizz_question_title" class="page-header">
-            <h4 id="question_header"><strong>QUESTION 1:</strong> WHAT IS THE FIRST STEP A USER MUST TAKE WHEN LOGGING INTO HPC?</h4>
+    <?php $question_num = 1; ?>
+    <!--START MAIN PHP LOOP FOR GENERATING QUESTIONS-->
+    <?php while($question = mysqli_fetch_assoc($question_set)) { ?>
+    <div class="quizz_question_div container question_card">
+        <div class="page-header">
+            <h4><strong>QUESTION <?php echo $question_num ?>:</strong> <?php echo h($question['question_text']) ?></h4>
         </div>
         <div class="questions_list_div">
-            <form id="question_form" action="process_answers.php" method="POST">
+            <form id="question_form_<?php echo h($question['question_id']) ?>" action="process_answers.php" method="POST">
                 <!--INNER FORM ELEMENTS GENERATED DYNAMICALLY WITH DATABASE (ALSO CHANGE CHECKBOX NUMVER DYNAMICALLY-->
+
+                <!--LOAD QUESTION CHOICES-->
+                <?php
+                    if ($question['question_multivalued'] == 1){
+                        $question_type_class = 'checkbox';
+                        $choice_name = 'check';
+                    } else{
+                        $question_type_class = 'radio';
+                        $choice_name = 'radio';
+                    }
+                ?>
+                <?php $choice_set = find_choices_by_question_id(h($question['question_id'])); ?>
+                <?php while($choice = mysqli_fetch_assoc($choice_set)) { ?>
+
                 <div class="question_item_div center">
-                    <div class="checkbox question_item">
-                        <span class=" glyphicon glyphicon-ok solution_glyphicon_correct"></span><label class="question_label"><input class="checkbox_item" type="checkbox" name="check_1" value="">Option 1</label>
+                    <div class="<?php echo $question_type_class ?> question_item">
+                        <!--USE CHOICE ID FOR THE NAME, REMOVE HIDDEN CLASS and add other classes WITH AJAX-->
+                        <span class="hidden glyphicon glyphicon-ok solution_glyphicon_correct"></span><label class="question_label"><input class="<?php echo $question_type_class ?>_item" type="<?php echo $question_type_class ?>" name="<?php echo $choice_name ?>_<?php echo h($question['question_id']) ?>" value=""><?php echo h($choice['question_choice_text']) ?></label>
                     </div>
                 </div>
-                <div class="question_item_div center">
-                    <div class="checkbox question_item">
-                        <span class=" glyphicon glyphicon-ok solution_glyphicon_correct"></span><label class="question_label"><input class="checkbox_item" type="checkbox" name="check_2" value="">Option 2</label>
-                    </div>
-                </div>
+
+                <?php } ?>
+
+
+<!--
                 <div class="question_item_div center">
                     <div class="checkbox question_item">
                         <span class=" glyphicon glyphicon-remove solution_glyphicon_incorrect"></span><label class="question_label"><input class="checkbox_item" type="checkbox" name="check_3" value="">Option 3</label>
                     </div>
                 </div>
-                <div class="question_item_div center">
-                    <div class="checkbox question_item">
-                        <span class=" glyphicon glyphicon-remove solution_glyphicon_incorrect"></span><label class="question_label"><input class="checkbox_item" type="checkbox" name="check_4" value="">Option 4</label>
-                    </div>
-                </div>
+-->
+
                 <br>
                 <hr>
+                <!--MAY WANT TO ADD THIS DIV DYNAMICALLY WITH AJAX-->
                 <div id="answer_explanations_div" class="well">
                     <div class='alert alert-danger'><strong>Answer 1: </strong>This answer is wrong due to bla bla bla bla</div>
                     <div class='alert alert-success'><strong>Answer 2: </strong>This answer is correct due to bla bla bla bla</div>
@@ -125,16 +140,16 @@
                 </div>
 
                 <div class="row bottom_button_set">
-                    <div id="previous_question_btn_div" class="col-xs-3">
+                    <div class="previous_question_btn_div col-xs-3">
                         <button id="previous_question_btn" type="button" class="btn btn-info">
                             <span class="glyphicon glyphicon-chevron-left"></span>
                         </button>
                     </div>
-                    <div id="view_answers_btn_div" class="col-xs-6">
+                    <div class="view_answers_btn_div col-xs-6">
                         <button id="view_answers_btn" class="btn btn-info btn-block" type="button">SUBMIT</button>
                     </div>
-                    <div id="next_question_btn_div" class="col-xs-3">
-                        <button id="next_question_btn" type="submit" class="btn btn-info">
+                    <div class="next_question_btn_div col-xs-3">
+                        <button id="next_question_btn" type="button" class="btn btn-info">
                             <span class="glyphicon glyphicon-chevron-right"></span>
                         </button>
                     </div>
@@ -144,6 +159,13 @@
         <br>
     </div>
     <hr>
+
+    <?php $question_num++; ?>
+    <!--end of question while loop-->
+    <?php } ?>
+
+
+
 
     <div id="quizz_progress_bar_div">
         <div class="progress">
